71096eb0cc8c966e586d1c90910dd26b7bbcaec5
Johnny Gu <johnny@wiredcraft.com>
2022-04-15 11:30:29 +0800

    fix: supported both xlsx and csv parse

--- src/utils/tokenParser.js
+++ src/utils/tokenParser.js
@@ -1,8 +1,5 @@
 const R = require("ramda");
-const csv = require("csv-parser");
-const fs = require("fs");
 var XLSX = require("xlsx");
-const path = require("path");
 
 const formatKey = (key) => {
   const removespace = key.trim().toLowerCase().replace(/[!@#$%^&*() , .?'":{}|<>]+/g, "_");
@@ -11,54 +8,30 @@ const formatKey = (key) => {
 
 const sortByKey = R.sortBy(R.prop('key'));
 
-const readFromCsv = (filepath) => {
-  const results = [];
+const readFromFile = (filepath) => {
   return new Promise((resolve, reject) => {
-    fs.createReadStream(filepath)
-      .pipe(csv())
-      .on("data", (data) => {
-        var data = JSON.parse(JSON.stringify(data))
-        console.log(data)
-        console.log(Object.keys(data))
+    try {
+      var workbook = XLSX.readFile(filepath);
+      var jsa = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]]);
+      var results = jsa.reduce((result, data) => {
         var { key, KEY, Key } = data;
         const safeKey = key || KEY || Key;
-        console.log(safeKey)
         if (safeKey && safeKey.length > 0) {
-          results.push({
+          result.push({
             ...data,
             key: formatKey(safeKey),
           });
         }
-      })
-      .on("end", () => {
-        console.log(results, filepath);
-        resolve(sortByKey(results));
-      })
-      .on("error", (err) => {
-        reject(err);
-      });
+        return result;
+      }, []);
+      resolve(sortByKey(results));
+    } catch (error) {
+      reject(error);
+    }
   });
 };
 
-const convertToCsv = (filepath) => {
-  const fileExtension = path.extname(filepath);
-  const dirname = path.dirname(filepath);
-  const basename = path.basename(filepath, ".xlsx"); 
-  console.log(filepath, fileExtension, dirname, basename)
-  if(fileExtension === ".csv") { 
-    return Promise.resolve(filepath); 
-  } else if(fileExtension === ".xlsx") {
-    var workbook = XLSX.readFile(filepath);
-    const csvFullPath = path.join(dirname, basename + '.csv');
-    XLSX.writeFile(workbook, csvFullPath);
-    return Promise.resolve(csvFullPath);
-    return
-  } else {
-    return Promise.reject("Invalid file format, it should be either csv or xlsx");
-  }
-}
-
-const parse = filepath => convertToCsv(filepath).then(path => readFromCsv(path))
+const parse = filepath => readFromFile(filepath)
 
 module.exports = {
   parse,
